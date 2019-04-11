deb2feeffaaff9a313b62d76a6a1b122747ecc1e
ed1son67 <1215373008@qq.com>
2019-04-12 00:11:05 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    
+  },
+  //事件处理函数
+  switchPage: function(e) {
+    let url = ['../report/report', '../suggestion/suggestion', '../center/center'];
+   
+    wx.navigateTo({
+      url: url[e.currentTarget.dataset.index]
+    })
+  },
+  onLoad: function () {
+    
+  },
+ 
+})
---
+++ pages/report/report.js
@@ -0,0 +1,100 @@
+// pages/report/report.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detailData: [
+      {
+        data: '30%',
+        title: '深睡比例'
+      },
+      {
+        data: '7h30min',
+        title: '入睡时长'
+      },
+      {
+        data: 0,
+        title: '心率突高次数'
+      },
+      {
+        data: '23:00',
+        title: '上床时间'
+      },
+      {
+        data: 12,
+        title: '呼吸速率突高次数'
+      },
+      
+      {
+        data: '24:00',
+        title: '入睡时间'
+      },
+      {
+        data: '7:40',
+        title: '清醒时间'
+      },
+      {
+        data: '8:00',
+        title: '起床时间'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
